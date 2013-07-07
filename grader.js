b04518e24cf5b6973af3d87390c6aee43278c404
martin.flores <martin.flores@bodybuilding.com>
2013-07-07 17:43:02 -0600

    Updated grader.js to support URLs as param with restler

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var DEFAULT_URL = "http://enigmatic-lowlands-1610.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,20 +57,60 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkHtmlFromUrl = function(html, checksfile) {
+    $ = cheerio.load(html);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+var getUrlContents = function (theUrl, checksfile){
+  rest.get(theUrl).on('complete', function (result){
+    if (result instanceof Error) {
+      console.error("Error: "+result.message);
+      process.exit(1);
+    }
+    
+    var checkJson = checkHtmlFromUrl(result, checksfile);
+    console.log(JSON.stringify(checkJson, null, 4));
+  });
+}
+
+var assertValidUrl = function (theUrl){
+  if(null == theUrl){
+    console.error("Invalid URL");
+    process.exit(1);
+  }
+  return theUrl   
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to the html file', clone(assertValidUrl))
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    var checkJson = "";
+    if(null != program.url){
+      console.log("From URL: "+program.url);
+      getUrlContents(program.url, program.checks);
+    } else {
+      console.log("From file: "+program.file);
+      checkJson = checkHtmlFile(program.file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson); 
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
