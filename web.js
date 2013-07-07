cfe59689e1a0ac24fea994403cfc2caf6a47021a
martin.flores <martin.flores@bodybuilding.com>
2013-07-06 23:09:07 -0600

    Displaying index.html content using buffers

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var inFile = 'index.html';
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var getInitialContent = function (){
+	var input = new fs.readFileSync(inFile);
+	var buffer = new Buffer(input, 'utf-8');
+	return buffer.toString();
+}
+
+app.get('/', function (request, response) {
+  response.send(getInitialContent());
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("Listening on " + port);
 });
